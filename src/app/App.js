44de1c81bde02dbe3ba23f10ecd7cf2f706cdaf4
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-13 15:59:57 +0000

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalculatorDisplay from './components/CalculatorDisplay';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/CalculatorDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class CalculatorDisplay extends Component {
+
+  render(){
+    return (
+      <>
+        <h4>Welcome to the calculator.</h4>
+      </>
+    );
+  }
+
+}
+
+export default CalculatorDisplay;
